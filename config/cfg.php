ee8ed3c25f47b33abe35ac196aafdcc3a5d241c6
GradenD <bacharevia@mail.ru>
2023-07-16 15:43:31 +0500

    removed gitignored files

--- config/cfg.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function script($txt, $class=false){
-        return "\n<div class='$class'>\n<script>\n" . $txt . "\n</script>\n</div>\n";
-    }
-    const __PAGES__ =  'pages/';
-    const __TMP__ =  'tmp/';
-    const __LIB__ = 'library/';
-    const __CFG__ = 'config/';
-?>
--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$test = "";
-    $host = 'localhost';
-    $dbname = 'bacharev_music';
-    $user = 'bacharev_music';
-    $password = '2a&MVow*';
-    $db = new mysqli($host, $user, $password, $dbname);
-?>
